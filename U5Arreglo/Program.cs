2bb05793860f513b908b7abef0862a38d16e025f
eherca0502 <eduardoicc@outlook.es>
2023-03-13 15:29:48 -0600

    Agregar archivos de proyecto.

---
+++ U5Arreglo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U5Arreglo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración e inicialización del arreglo
+            int[] numeros = { 10, 5, 20, 15, 3, 8 };
+
+            // Inicialización de variables
+            int maximo = numeros[0];
+            int minimo = numeros[0];
+
+            // Recorrido del arreglo
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                // Actualización del valor máximo
+                if (numeros[i] > maximo)
+                {
+                    maximo = numeros[i];
+                }
+                // Actualización del valor mínimo
+                if (numeros[i] < minimo)
+                {
+                    minimo = numeros[i];
+                }
+            }
+            // Impresión de resultados
+            Console.WriteLine("El número mayor es: " + maximo);
+            Console.WriteLine("El número menor es: " + minimo);
+        }
+    }
+}
+   
